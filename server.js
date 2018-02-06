64983a85831bf428211300885d385120349a1054
Eric Zhao <zhao.yichao15@gmail.com>
2018-02-07 08:54:35 +1100

    add temperary redirection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/redirect', (req, res) => res.redirect(301, 'https://www.oneu.me/'))
+app.get('/', (req, res) => res.redirect(302, 'https://www.oneu.me/'))
 var port = process.env.PORT || 3000;
 app.listen(port);
